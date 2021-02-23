93b92cc672363a8ce900ff4644ffe3deb5e47658
Elizabeth Geraci <egeraci@bsd220.org>
2021-02-23 16:24:10 -0600

    Initial Commit

---
+++ Assignment Notebook Redo/Assignment_Notebook_RedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_Notebook_RedoApp.swift
+//  Assignment Notebook Redo
+//
+//  Created by Student on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_Notebook_RedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment Notebook Redo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment Notebook Redo
+//
+//  Created by Student on 2/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
