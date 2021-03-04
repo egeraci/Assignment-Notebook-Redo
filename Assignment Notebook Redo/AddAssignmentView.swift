e3cf03598bec5dcb0a2900ae4171a170ab619fd0
Elizabeth Geraci <egeraci@bsd220.org>
2021-03-04 16:28:24 -0600

    Created add assignment view

---
+++ Assignment Notebook Redo/AddAssignmentView.swift
@@ -0,0 +1,38 @@
+//
+//  AddAssignmentView.swift
+//  Assignment Notebook Redo
+//
+//  Created by Student on 3/4/21.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    @ObservedObject var assignmentList: AssignmentList
+    @State private var className = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presantationMode
+    static let classNames = ["Mobile Apps", "Art", "English", "PE", "Dance", "Drama", "Math","BHS-TV", "Lunch", "Civics"]
+    var body: some View {
+        NavigationView
+        {
+            Form
+            {
+                Picker ("Class Name", selection: $className)
+                {
+                    ForEach (Self.classNames, id:\.self)
+                        {
+                            className in Text (className)
+                        }
+                }
+            }
+        }
+    }
+}
+
+struct AddAssignmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddAssignmentView(assignmentList: AssignmentList())
+    }
+}
