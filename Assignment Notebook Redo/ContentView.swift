657e757c1e43befef6696da57265748b33924aa1
Elizabeth Geraci <egeraci@bsd220.org>
2021-03-04 15:51:30 -0600

    Created initial assignment list

--- Assignment Notebook Redo/ContentView.swift
+++ Assignment Notebook Redo/ContentView.swift
@@ -8,16 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-   @State var assignments = ["assignment1", "assignment2", "assignment3"]
+   @State var assignments = [AssignmentItem ( className: "Drama", discription:"line reading", dueDate: Date()), AssignmentItem(className: "English", discription: "writing a paper", dueDate: Date())]
     
     var body: some View
     {
         NavigationView {
                     List {
 
-                       ForEach(assignments, id: \.self)
+                       ForEach(assignments)
                         {
-                            assignment in Text(assignment)
+                        assignment in Text(assignment.discription)
                          }
                        .onMove(perform: { indices, newOffset in
                         assignments.move(fromOffsets: indices, toOffset: newOffset)
@@ -38,4 +38,12 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var className = String()
+    var discription = String()
+    var dueDate = Date()
+    
 }
