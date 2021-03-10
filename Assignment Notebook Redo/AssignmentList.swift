6373ac0e8f417eb2c1fc70da94c3f9c5fa5a7b40
Elizabeth Geraci <egeraci@bsd220.org>
2021-03-10 15:42:40 -0600

    FINISHED!!!!!!! ICECREAM FACE.

--- Assignment Notebook Redo/AssignmentList.swift
+++ Assignment Notebook Redo/AssignmentList.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 class AssignmentList: ObservableObject {
-    @Published var assignment = [AssignmentItem ( className: "Drama", discription:"line reading", dueDate: Date()), AssignmentItem(className: "English", discription: "writing a paper", dueDate: Date())]
+    @Published var assignment: [AssignmentItem]
+    {
+        didSet
+        {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(assignment)
+            {
+                UserDefaults.standard.set(encoded, forKey: "data")
+            }
+        }
+    }
+    
+    init()
+    {
+        if let assignment = UserDefaults.standard.data(forKey: "data")
+        {
+                let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([AssignmentItem].self, from: assignment)
+            {
+                self.assignment = decoded
+                return
+            }
+        }
+        assignment = []
+    }
 }
